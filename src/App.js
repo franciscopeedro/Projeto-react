3bb743c0093183dc42d2385f6b0d62dd10b1fbdf
Francisco Pedro <francisco.pedro@spotconn.com.br>
2023-02-23 15:20:53 -0300

    projeto 3D

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import { AiOutlinePlus } from "react-icons/ai";
 import { AiOutlineMinus } from "react-icons/ai";
 import { FaCheck } from "react-icons/fa";
-import {FaExclamation} from "react-icons/fa";
-import {FaSquareFull} from "react-icons/fa";
-import {FaTh} from "react-icons/fa";
-import {FaRegStarHalf} from "react-icons/fa";
+import { FaExclamation } from "react-icons/fa";
+import { FaSquareFull } from "react-icons/fa";
+import { FaTh } from "react-icons/fa";
+import { FaRegStarHalf } from "react-icons/fa";
 import { useState } from 'react';
+import { Viewer, XKTLoaderPlugin } from "./services/api";
 function App() {
 
   const [openLink, setOpenLink] = useState(false);
@@ -19,37 +20,38 @@ function App() {
     <div className='tudo'>
       <div className='cabecalho'>OD:950.00.mm | WHT:35.00mm | ELV:-14.0m </div>
       <div className='backmenu'>
-      <div className='menu backmenu'>
-        <ul>
-          <button className='semlinha' onClick={() => setOpenLink(!openLink)}> {openLink ? <AiOutlineMinus size={15} color="black" /> : <AiOutlinePlus size={15} color="black" />}Workplan</button>
-          {openLink && <>
-            <li> <button className='semlinha espaco'>pag1</button></li>
-            <li> <button className='semlinha espaco'>pag2</button></li>
-            <li> <button className='semlinha espaco'>pag3</button></li>
-          </>}
-        </ul>
+        <div className='menu backmenu'>
+          <ul>
+            <button className='semlinha' onClick={() => setOpenLink(!openLink)}> {openLink ? <AiOutlineMinus size={15} color="black" /> : <AiOutlinePlus size={15} color="black" />}Workplan</button>
+            {openLink && <>
+              <li> <button className='semlinha espaco'>pag1</button></li>
+              <li> <button className='semlinha espaco'>pag2</button></li>
+              <li> <button className='semlinha espaco'>pag3</button></li>
+            </>}
+          </ul>
 
-        <ul>
-          <button className='semlinha' onClick={() => setOpenLink2(!openLink2)}> {openLink2 ? <AiOutlineMinus size={15} color="black" /> : <AiOutlinePlus size={15} color="black" />}Workplan</button>
-          {openLink2 && <>
-            <li><button className='semlinha espaco'>pag1</button></li>
-            <li><button className='semlinha espaco'>pag2</button></li>
-            <li><button className='semlinha espaco'>pag3</button></li>
-          </>}
-        </ul>
+          <ul>
+            <button className='semlinha' onClick={() => setOpenLink2(!openLink2)}> {openLink2 ? <AiOutlineMinus size={15} color="black" /> : <AiOutlinePlus size={15} color="black" />}Workplan</button>
+            {openLink2 && <>
+              <li><button className='semlinha espaco'>pag1</button></li>
+              <li><button className='semlinha espaco'>pag2</button></li>
+              <li><button className='semlinha espaco'>pag3</button></li>
+            </>}
+          </ul>
 
-        <ul>
-          <button className='semlinha' onClick={() => setOpenLink3(!openLink3)}> {openLink3 ? <AiOutlineMinus size={15} color="black" /> : <AiOutlinePlus size={15} color="black" />}Workplan</button>
-          {openLink3 && <>
-            <li><button className='semlinha espaco'>pag1</button></li>
-            <li><button className='semlinha espaco'>pag2</button></li>
-            <li><button className='semlinha espaco'>pag3</button></li>
-          </>}
-        </ul>
+          <ul>
+            <button className='semlinha' onClick={() => setOpenLink3(!openLink3)}> {openLink3 ? <AiOutlineMinus size={15} color="black" /> : <AiOutlinePlus size={15} color="black" />}Workplan</button>
+            {openLink3 && <>
+              <li><button className='semlinha espaco'>pag1</button></li>
+              <li><button className='semlinha espaco'>pag2</button></li>
+              <li><button className='semlinha espaco'>pag3</button></li>
+            </>}
+          </ul>
+        </div>
       </div>
+      <div className="principal"><video className="video" controls autoplay src="https://opexeng.com.br/assets/videos/banner_home.mp4"></video></div>
+      <div className='projeto3D'>
       </div>
-      <div className="principal"><video className="video"controls autoplay src="https://opexeng.com.br/assets/videos/banner_home.mp4"></video></div>
-      <div className='lado'></div>
       <div className="modo">
         <button className='letra-azul semlinha'>Workplan |</button>
         <button className='letra-azul semlinha'>Components |</button>
@@ -58,13 +60,13 @@ function App() {
         <button className='letra-azul semlinha'>Video Index</button>
       </div>
       <div className='opcoes'>
-        <button className='letra-azul semlinha'> <FaCheck size={16} color='green'/> Reviwed:0 |</button>
-        <button className='letra-azul semlinha'> <FaExclamation size={16} color="red"/> Pending:80 |</button>
-        <button className='letra-azul semlinha'> <FaSquareFull size={12} color="green"/> Open:80 |</button>
-        <button className='letra-azul semlinha'> <FaSquareFull size={12} color="red"/> Closed:0 |</button>
-        <button className='letra-azul semlinha'> <FaSquareFull size={12} color="yellow"/> Recommended By Inspector:80 |</button>
-        <button className='letra-azul semlinha'> <FaTh size={12} color="black"/> Color in 3D |</button>
-        <button className='letra-azul semlinha'> <FaRegStarHalf size={16} color="black"/>Select Fitered in 3D</button>
+        <button className='letra-azul semlinha'> <FaCheck size={16} color='green' /> Reviwed:0 |</button>
+        <button className='letra-azul semlinha'> <FaExclamation size={16} color="red" /> Pending:80 |</button>
+        <button className='letra-azul semlinha'> <FaSquareFull size={12} color="green" /> Open:80 |</button>
+        <button className='letra-azul semlinha'> <FaSquareFull size={12} color="red" /> Closed:0 |</button>
+        <button className='letra-azul semlinha'> <FaSquareFull size={12} color="yellow" /> Recommended By Inspector:80 |</button>
+        <button className='letra-azul semlinha'> <FaTh size={12} color="black" /> Color in 3D |</button>
+        <button className='letra-azul semlinha'> <FaRegStarHalf size={16} color="black" />Select Fitered in 3D</button>
       </div>
       <div className='tabela'>
         <table class="table table-bordered">
@@ -173,11 +175,6 @@ function App() {
           </tbody>
         </table>
       </div>
-
-
-
-
-
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,47 @@
+import {Viewer, XKTLoaderPlugin} from "https://xeokit.github.io/xeokit-bim-viewer/app/index.html?projectId=OTCConferenceCenter&tab=storeys";
+
+const viewer = new Viewer({
+    canvasId: "myCanvas"
+});
+
+viewer.camera.eye = [-3.93, 2.85, 27.01];
+viewer.camera.look = [4.40, 3.72, 8.89];
+viewer.camera.up = [-0.01, 0.99, 0.039];
+
+const xktLoader = new XKTLoaderPlugin(viewer);
+
+const sceneModel = xktLoader.load({
+    id: "myModel",
+    src: "../assets/models/xkt/v10/glTF-Embedded/Duplex_A_20110505.glTFEmbedded.xkt", // Creates a MetaObject instances in scene.metaScene.metaObjects
+    edges: true
+});
+
+var lastEntity = null;
+
+viewer.scene.input.on("mousemove", function (coords) {
+
+    var hit = viewer.scene.pick({
+        canvasPos: coords
+    });
+
+    if (hit) {
+
+        if (!lastEntity || hit.entity.id !== lastEntity.id) {
+
+            if (lastEntity) {
+                lastEntity.selected = false;
+            }
+
+            lastEntity = hit.entity;
+            hit.entity.selected = true;
+        }
+    } else {
+
+        if (lastEntity) {
+            lastEntity.selected = false;
+            lastEntity = null;
+        }
+    }
+});
+
+export default {viewer,xktLoader};
